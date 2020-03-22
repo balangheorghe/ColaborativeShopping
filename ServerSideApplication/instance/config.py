e40dd1d478114ba35e3902e4283799acbc6118b4
Gheorghe BALAN <gbalan@ias.bitdefender.biz>
2020-03-22 18:42:26 +0200

    added .gitignore

---
+++ ServerSideApplication/instance/config.py
@@ -0,0 +1,9 @@
+app.config['SOCIAL_FACEBOOK'] = {
+    'consumer_key': '688309955244225',
+    'consumer_secret': '1235943d4d3915b4d3889d130d0900ce'
+}
+
+app.config['SOCIAL_GOOGLE'] = {
+    'consumer_key': 'xxxx',
+    'consumer_secret': 'xxxx'
+}
