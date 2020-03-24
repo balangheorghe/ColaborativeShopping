0392b87e91fda396ea2117e0d4cab36d95522e7c
Gheorghe BALAN <gbalan@ias.bitdefender.biz>
2020-03-25 00:23:48 +0200

    updates

--- ServerSideApplication/app/app/settings.py
+++ ServerSideApplication/app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'p8a30n-obvsio_^oc$znztg4*1vx%cf)ig=6dqn2!30ghml4rv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["colaborativeshopping.herokuapp.com"]
 
 # Application definition
 
@@ -169,3 +170,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
